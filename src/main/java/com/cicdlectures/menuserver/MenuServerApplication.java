a0e78bc32bb414411ccfcf054b5d7d5a379f045d
Damien D'ARRAS <damien.darras44@gmail.com>
2023-03-14 10:45:20 +0000

    retour

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MenuSererApplication.class, args);
+    SpringApplication.run(MenuServerApplication.class, args);
   }
 
 }
